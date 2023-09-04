cf2d694f377f1c7288df93dd96ba7c5dcad75045
Yamikani Chinamale <ychinamale@gmail.com>
2023-09-04 15:23:51 +0200

    First commit

---
+++ android/app/src/main/java/com/sumsubionic/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.sumsubionic.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.sumsubionic.app',
   appName: 'sumsub-ionic',
   webDir: 'www',
   server: {
