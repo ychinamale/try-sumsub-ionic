82db689e2e985d97458f2d6529213cbfe1bbdc56
Yamikani Chinamale <ychinamale@gmail.com>
2023-09-08 00:54:16 +0200

    Removed excess code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,30 +13,10 @@ import { SNSMobileSDK } from '@sumsub/cordova-idensic-mobile-sdk-plugin';
 export class HomePage {
   constructor() {}
 
-  launchSumsub() {
-    let accessToken = 'your_access_token';
-
-    // NOTE commenting this 👇🏾 out makes the home page appear
-    // leaving this here makes the home page crash
-    let snsMobileSDK = SNSMobileSDK.init(accessToken, () => {
-        return new Promise((resolve, reject) => {
-            resolve('new_access_token')
-        })
-      })
-    // NOTE commented out until I figure out why SNSMobileSDK.init() is freaking out
-    //   .withHandlers({ // Optional callbacks you can use to get notified of the corresponding events
-    //     onStatusChanged: (event) => {
-    //       console.log("onStatusChanged: [" + event.prevStatus + "] => [" + event.newStatus + "]");
-    //     }
-    //   })
-    //   .withDebug(true)
-    //   .withLocale('en') // Optional, for cases when you need to override the system locale
-    //   .build();
-
-    // snsMobileSDK.launch().then((result: any) => {
-    //   console.log("Sumsub SDK State: " + JSON.stringify(result))
-    // }).catch((err: any) => {
-    //   console.log("Sumsub SDK Error: " + JSON.stringify(err))
-    // });
+  /**
+   * This is a method that is never called in this project */
+  neverCalledEver() {
+    // 👇🏾 but this code still manages to crash the home page somehow.
+    SNSMobileSDK.init();
   }
 }
